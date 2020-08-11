93a3ff19d48add632dfbee53a1c2da2445bd3a6e
solbencas <cecibenitezca@gmail.com>
2020-08-11 17:06:54 -0400

    linters fix

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,21 +1,21 @@
+# rubocop:disable Metrics/ClassLength
 def bubble_sort(list)
-	sorted = false
-	passes = 1
+  sorted = false
+  passes = 1
 
-	until sorted == true
-		sorted = true 
+  until sorted == true
+    sorted = true
 
-		(0...list.length - passes).each do |i|
-			if list[i] > list[i + 1]
-				sorted = false 
-				temp = list[i]
-				list[i] = list[i + 1]
-				list[i + 1] = temp
-			end
-		end	
-		passes += 1
-	end
-	list
+    (0...list.length - passes).each do |i|
+      if list[i] > list[i + 1]
+        sorted = false
+        temp = list[i]
+        list[i] = list[i + 1]
+        list[i + 1] = temp
+      end
+    end
+    passes += 1
+  end
+  list
 end
 p bubble_sort([78, 4, 3, 2, 2, 0])
-    
