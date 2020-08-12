0a8a5aa07e3156a426ca720c310f46e594d7d289
solbencas <cecibenitezca@gmail.com>
2020-08-12 12:08:44 -0400

    add bubble_sort_by.rb file

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,10 +1,21 @@
 def bubble_sort(list)
+  list = bubble_sort_by(list) do |left, right|
+    left < right
+  end
+  list
+end
+
+def bubble_sort_by(list)
   sorted = false
   passes = 1
   until sorted == true
     sorted = true
     (0...list.length - passes).each do |i|
-      next unless list[i] > list[i + 1]
+      block_results = yield(list[i], list[i + 1])
+
+      block_results = block_results >= 1 if block_results.is_a? Integer
+
+      next unless block_results
 
       sorted = false
       temp = list[i]
